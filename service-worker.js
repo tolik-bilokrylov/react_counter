a8f312949e3d05430b41e6f6622e6e53c4dc3641
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-07-22 18:40:15 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.cea113b599a4e3f0867f8878c17eb6ea.js"
 );
 
 self.addEventListener('message', (event) => {
